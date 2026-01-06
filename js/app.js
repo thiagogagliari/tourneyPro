78d5542e0927e6ae96b9e02054355ed7b7992c48
thiagogagliari <thiagogagliari@hotmail.com>
2026-01-06 12:54:06 -0300

    update

--- js/app.js
+++ js/app.js
@@ -4327,6 +4327,7 @@ class TournamentManager {
       Congo: "https://flagcdn.com/w20/cg.png",
       Tanzânia: "https://flagcdn.com/w20/tz.png",
       Uganda: "https://flagcdn.com/w20/ug.png",
+      "Sudão do Sul": "https://flagcdn.com/w20/ss.png",
       UEFA: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZP1mQZa3mJngyadCmDM6KFijvemaeL21h8w&s",
       CONMEBOL:
         "https://www.vhv.rs/dpng/d/417-4177504_conmebol-logo-png-conmebol-logo-transparent-png.png",
